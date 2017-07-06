669d4733a3e91342e8bd90011bae8201c3e96226
Miguel Muscat <miguelmuscat93@gmail.com>
2017-07-06 15:45:27 +0200

    Added `NonceFactory`

---
+++ src/Factory/NonceFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace RebelCode\WordPress\Nonce\Factory;
+
+use Dhii\Factory\FactoryInterface;
+use RebelCode\WordPress\Nonce\Nonce;
+use RebelCode\WordPress\Nonce\NonceInterface;
+
+/**
+ * The simple, default implementation of a factory that can create nonce instances.
+ *
+ * @since [*next-version*]
+ */
+class NonceFactory extends AbstractNonceFactory implements FactoryInterface
+{
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Creates  a new nonce instance.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $id     The ID of the nonce.
+     * @param array  $config Optional additional arguments.
+     *
+     * @return NonceInterface
+     */
+    public function make($id, array $config = [])
+    {
+        return $this->_make($id, $config);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    protected function _createNonce($id, $code)
+    {
+        return new Nonce($id, $code);
+    }
+}
---
+++ test/unit/Factory/NonceFactoryTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace unit\Factory;
+
+use RebelCode\WordPress\Nonce\Factory\NonceFactory;
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see RebelCode\WordPress\Nonce\Factory\NonceFactory}.
+ *
+ * @since [*next-version*]
+ */
+class NonceFactoryTest extends TestCase
+{
+    /**
+     * Tests whether a valid instance can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = new NonceFactory();
+
+        $this->assertInstanceOf(
+            'Dhii\\Factory\\FactoryInterface', $subject,
+            'Subject does not implement expected interface.'
+        );
+    }
+
+    /**
+     * Tests the nonce creation method to ensure that the created nonce is a valid nonce instance.
+     *
+     * @since [*next-version*]
+     *
+     */
+    public function testMakeInstanceType()
+    {
+        $subject = new NonceFactory();
+        $nonce   = $subject->make('my-nonce');
+
+        $this->assertInstanceOf(
+            'RebelCode\\WordPress\\Nonce\\NonceInterface', $nonce,
+            'Created instance is not a valid nonce instance.'
+        );
+    }
+
+    /**
+     * Tests the nonce creation method to ensure that the created nonce has an ID equal to the argument given.
+     *
+     * @since [*next-version*]
+     */
+    public function testMakeNonceId()
+    {
+        $subject = new NonceFactory();
+        $nonce   = $subject->make($id = 'my-new-nonce');
+
+        $this->assertEquals($id, $nonce->getId());
+    }
+}
