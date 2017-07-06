dd7d6f8a8b921370758b8010fd75e1e583204212
Miguel Muscat <miguelmuscat93@gmail.com>
2017-07-06 14:40:35 +0200

    Added `AbstractNonceValidator`

---
+++ src/Validator/AbstractNonceValidator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace RebelCode\WordPress\Nonce\Validator;
+
+use Dhii\I18n\StringTranslatingTrait;
+use Dhii\Validation\AbstractValidatorBase;
+use RebelCode\WordPress\Nonce\NonceInterface;
+
+/**
+ * Basic functionality for something that can validate nonces.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractNonceValidator extends AbstractValidatorBase
+{
+    use StringTranslatingTrait;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    protected function _getValidationErrors($subject)
+    {
+        if (!$subject instanceof NonceInterface) {
+            return [
+                $this->__('Subject is not a nonce instance.')
+            ];
+        }
+
+        if (!$this->_verifyNonce($subject)) {
+            return [
+                $this->__('Subject nonce instance is not valid, or is expired.')
+            ];
+        }
+
+        return [];
+    }
+
+    /**
+     * Verifies that a nonce is correct and unexpired.
+     *
+     * @since [*next-version*]
+     *
+     * @param NonceInterface $nonce The nonce instance to verify.
+     *
+     * @return bool True if the nonce is correct and unexpired, false otherwise.
+     */
+    abstract protected function _verifyNonce(NonceInterface $nonce);
+}
