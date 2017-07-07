76437c767badeaca28155dea39eb5a1aa7882088
Miguel Muscat <miguelmuscat93@gmail.com>
2017-07-07 17:30:22 +0200

    Removed the WordPress stub functions file

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -5,5 +5,3 @@ error_reporting(E_ALL | E_STRICT);
 require_once dirname(__FILE__).'/../vendor/autoload.php';
 
 WP_Mock::bootstrap();
-
-require_once dirname(__FILE__).'/stub/wordpress-functions.php';
--- test/stub/wordpress-functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function wp_verify_nonce($code, $id = -1) {
-    $expected = \wp_create_nonce($id);
-
-    return hash_equals($expected, $code);
-}
